585d58dcf2c39eef6e7b96d416fcaf4c532e2dcb
PatrykPorabik <patryk@porabik.pl>
2020-08-20 10:56:55 +0200

    Implement manifest for plugin info - further changes

---
+++ Plugins/Grand.Plugin.Payments.PayInStore/Manifest.cs
@@ -0,0 +1,11 @@
+﻿using Grand.Core.Infrastructure;
+
+[assembly: PluginInfo(
+    FriendlyName = "Pay In Store",
+    Group = "Payment methods",
+    SystemName = "Payments.PayInStore",
+    SupportedVersion = "4.80",
+    Author = "grandnode team",
+    Version = "1.08",
+    FileName = "Grand.Plugin.Payments.PayInStore.dll"
+)]
---
+++ Plugins/Grand.Plugin.Tax.FixedRate/Manifest.cs
@@ -0,0 +1,11 @@
+﻿using Grand.Core.Infrastructure;
+
+[assembly: PluginInfo(
+    FriendlyName = "Fixed tax rate provider",
+    Group = "Tax providers",
+    SystemName = "Tax.FixedRate",
+    SupportedVersion = "4.80",
+    Author = "grandnode team",
+    Version = "1.15",
+    FileName = "Grand.Plugin.Tax.FixedRate.dll"
+)]
